10c09dd1fcd3ab75c4073488aba8d761e9157171
SergioPrieto <serprigar@gmail.com>
2024-05-09 11:13:19 +0200

    feat(Elipse.java): add class

---
+++ src/main/java/edu/badpals/FigurasGeometricas/Elipse.java
@@ -0,0 +1,32 @@
+package edu.badpals.FigurasGeometricas;
+
+public class Elipse extends FiguraGeometrica{
+    private double ejeMayor=0d;
+    private double ejeMenor=0d;
+
+    public Elipse(String nombre, double ejeMenor, double ejeMayor) {
+        super(nombre);
+        this.ejeMenor = ejeMenor;
+        this.ejeMayor = ejeMayor;
+    }
+
+    public Elipse(double ejeMenor, double ejeMayor) {
+        super();
+        this.ejeMenor = ejeMenor;
+        this.ejeMayor = ejeMayor;
+    }
+
+    public double getEjeMayor() {
+        return ejeMayor;
+    }
+
+    public double getEjeMenor() {
+        return ejeMenor;
+    }
+
+
+    @Override
+    public double area() {
+        return Math.PI*getEjeMayor()*getEjeMenor();
+    }
+}
