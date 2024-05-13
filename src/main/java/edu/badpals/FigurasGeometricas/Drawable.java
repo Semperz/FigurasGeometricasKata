b049802eb2b75573dcd8817f635ce351307accd1
SergioPrieto <serprigar@gmail.com>
2024-05-13 08:49:19 +0200

    feat(Drawable.java): add message to Exception

--- src/main/java/edu/badpals/FigurasGeometricas/Drawable.java
+++ src/main/java/edu/badpals/FigurasGeometricas/Drawable.java
@@ -2,7 +2,7 @@ package edu.badpals.FigurasGeometricas;
 
 public interface Drawable {
     default void applyTheme(){
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("Operacion applyTheme sin implementar");
     }
     void draw();
 
